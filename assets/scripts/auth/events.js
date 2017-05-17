12971f27e7d95d61c7fdd917b4da57c4e5871c28
joehsuLM <joseph.hsu@libertymutual.com>
2017-05-17 11:15:54 -0400

    Changed how modals flow for sign-up and sign-in
    When a user without an account reaches the sign-in modal and clicks
    'Create a new account', the sign-in modal closes and the sign-up modal
    is displayed. When a user successfully signs-up, they are automatically
    signed in and the sign-up modal closes.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -45,9 +45,9 @@ const addAuthHandlers = () => {
   // $('#sign-in-modal-btn').on('click', function () {
   //   $('.auth-status').hide()
   // })
-  // $('#sign-up-modal-trigger-link').on('click', function () {
-  //   $('#sign-in-modal').modal('hide')
-  // })
+  $('#sign-up-modal-trigger-link').on('click', function () {
+    $('#sign-in-modal').modal('hide')
+  })
   // $('#change-password-modal-link').on('click', function () {
   //   $('#change-password-modal').modal('toggle')
   //   $('.auth-status').hide()
