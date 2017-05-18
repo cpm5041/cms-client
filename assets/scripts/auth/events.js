7a98d8af00463d18f1e41d5a7a4eb3fad27ac631
rrdaniels85 <rachel.daniels@libertymutual.com>
2017-05-18 18:31:08 -0400

    Updated comments for auth event functions

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -6,37 +6,49 @@ const api = require('./api')
 const ui = require('./ui')
 
 const onSignUp = function (event) {
+  // assign input from form fields to data
   const data = getFormFields(event.target) // 'this' is reffering to event.target
+  // prevent default action
   event.preventDefault()
+  // send ajax request
   api.signUp(data)
     .then(ui.signUpSuccess)
     .catch(ui.signUpFailure)
 }
 
 const onSignIn = function (event) {
+  // prevent default action
   event.preventDefault()
+  //  assign input from form fields to data
   const data = getFormFields(event.target) // 'this' is reffering to event.target
+  // send ajax request
   api.signIn(data)
     .then(ui.signInSuccess)
     .catch(ui.signInFailure)
 }
 
 const onSignOut = function (event) {
+  // prevent default action
   event.preventDefault()
+  // send ajax request
   api.signOut()
     .then(ui.signOutSuccess)
     .catch(ui.signOutFailure)
 }
 
 const onChangePassword = function (event) {
-  console.log('change pass click is heard')
+  // prevent default action
   event.preventDefault()
+  // assign input information to data
   const data = getFormFields(event.target)
   console.log('data is', data)
+  // send ajax request
   api.changePassword(data)
     .then(ui.changePasswordSuccess)
     .catch(ui.changePasswordFailure)
 }
+
+// event handlers for auth events
 const addAuthHandlers = () => {
   $('#sign-up').on('submit', onSignUp)
   $('#sign-in').on('submit', onSignIn)
