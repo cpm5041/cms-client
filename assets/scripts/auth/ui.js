40f9eae4a8949ae48380e812e8cbf27072d8e4c7
joehsuLM <joseph.hsu@libertymutual.com>
2017-05-18 21:24:04 -0400

    Removed commented code and console logs

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,12 +2,10 @@
 
 const store = require('../store')
 const api = require('./api')
-// const showItemsTemplate = require('../templates/item-listing-grid.handlebars')
 
 const signUpSuccess = (data) => {
   // store user information
   store.user = data.user
-  console.log('signup success')
   // store values from sign up input fields
   const credentials = {
     email: $('#sign-up-email').val(),
@@ -27,22 +25,17 @@ const signUpSuccess = (data) => {
 }
 
 const signUpFailure = (error) => {
-  console.log('sign up fail')
-  console.log(error)
   // clear input values
   $('#sign-up-email').val('')
   $('#sign-up-password').val('')
   $('#sign-up-password-confirmation').val('')
   // notify user of issue
   $('#sign-up-error').text('You may have entered invalid credentials, please try again.')
-  // $('#sign-up-status').show()
 }
 
 const signInSuccess = (data) => {
-  console.log('sign in success, data: ', data)
   // store user data
   const userEmail = data.user.email
-  console.log(userEmail)
   store.user = data.user // this stores the entire user object
   // welcome user message
   $('#welcomeEmail').html('Welcome ' + userEmail)
@@ -57,14 +50,11 @@ const signInSuccess = (data) => {
 }
 
 const signInFailure = (error) => {
-  console.log('sign in fail')
   // clear input fields
   $('#sign-in-email').val('')
   $('#sign-in-password').val('')
   // notify user of sign in failure with message
   $('#sign-in-error').text('You may have entered invalid credentials, please try again.')
-  console.log(error)
-  // $('#sign-in-status').show()
 }
 
 const signOutSuccess = () => {
@@ -72,7 +62,6 @@ const signOutSuccess = () => {
   store.user = null
   // remove user welcome message
   $('#welcomeEmail').html('')
-  console.log('sign out success')
   // adjust hidden classes
   $('.visitor-view').removeClass('hidden')
   $('.user-view').addClass('hidden')
@@ -82,27 +71,15 @@ const signOutSuccess = () => {
   $('#recent-pages').text('')
   $('#recent-pages').removeClass('active')
   $('#showPageButton').removeClass('active')
-  // $('#toggleClass').addClass('active')
-  // $('.visitorPageDiv').removeClass('active')
-
-  // $('.splash-jumbo').show()
-  // $('nav').hide()
-  // $('.item-jumbo').hide()
-  // $('#bs-example-navbar-collapse-1').collapse('hide')
-  // $('#collapseTwo').collapse('hide')
-  // $('.item_input').val('')
-  // ADD THE TABS ID TO THE HTML ELEMENT!!!!
   $('#tabs a[href="#create-blog"]').tab('show')
   $('#tabs a[href="#recent-blogs"]').tab('show')
 }
 
 const signOutFailure = (error) => {
-  console.log('sign out fail')
-  console.log(error)
+
 }
 
 const changePasswordSuccess = (data) => {
-  console.log('change pass success')
   // clear input fields
   $('#old-password').val('')
   $('#new-password').val('')
@@ -111,10 +88,8 @@ const changePasswordSuccess = (data) => {
 }
 
 const changePasswordFailure = (error) => {
-  console.log('change pass fail')
   // notify user of unsuccessful password change with message
   $('#change-password-failure').text('You may have an invalid password, please try again.')
-  console.log(error)
 }
 
 module.exports = {
